ab7db597cee5f963b211c18b16b1276c6ea68d99
Hassan-Qwashty <hqwashty@gmail.com>
2021-09-29 17:46:51 +0200

    Delete free spaces between Docstring

--- bikeshare.py
+++ bikeshare.py
@@ -9,7 +9,6 @@ CITY_DATA = { 'chicago': 'chicago.csv',
 def get_filters():
     """
     Asks user to specify a city, month, and day to analyze.
-
     Returns:
         (str) city - name of the city to analyze
         (str) month - name of the month to filter by, or "all" to apply no month filter
@@ -50,8 +49,7 @@ def get_filters():
 def load_data(city, month, day):
     """
     Loads data for the specified city and filters by month and day if applicable.
-
-    Args:
+     Args:
         (str) city - name of the city to analyze
         (str) month - name of the month to filter by, or "all" to apply no month filter
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
